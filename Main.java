2ee77a1d65dd2865af9004407d2ce2f5df930cbc
Nitin Gouda <90847875+shaggyyy2002@users.noreply.github.com>
2021-09-29 18:00:48 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,199 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+ public static void main(String[] args) {
+   /*  int marks = 18;
+     float averageMarks = 19.320f;
+     char grade = 'A';
+     String name = "Nitin";
+     String Sentence = " Nitin is a boy ";
+     boolean isValid = false;
+
+     long bigNumber = 9987724866l;
+     System.out.println(averageMarks);
+
+                                         // USING OPERATORS SYSTEM- ARITHMETIC OPERATORS //
+                                                     // ADDITION OPERATOR
+
+     int firstNumber = 346;
+     int secondNumber = 234;
+     int sum = firstNumber + secondNumber;
+
+     System.out.println(sum);
+
+                                                    // SUBTRACTION OPERATOR
+
+     int s1 = 352;
+     int s2 = 200;
+     int subtract = s1 - s2;
+
+     System.out.println(subtract);
+
+                                                  // MULTIPLICATION OPERATOR
+
+     int m1 = 9;
+     int m2 = 8;
+     int multiply = m1 * m2;
+
+     System.out.println(multiply);
+
+                                                     // DIVISION OPERATOR
+
+     int d1 = 35;
+     int d2 = 5;
+     int divide = d1 / d2;
+
+     System.out.println(divide);
+
+                                                       // MODULAR OPERATOR
+                                  // modular operators divide the number and gives the remainder //
+
+     int n1 = 26;
+     int n2 = 20;
+     int modular = n1 % n2;
+
+     System.out.println(modular);
+
+                                                    // INCREMENT OPERATORS (++)
+                                             // Increment operators adds +1 in given number
+
+     int c1 = 12;
+     c1++;
+
+     System.out.println(c1);
+
+                                                   // DECREMENT OPERATORS (--)
+                                          // Decrement operators subtract -1 in given number
+
+     int r1 = 14;
+     r1--;
+
+     System.out.println(r1);
+
+                                                       // Taking user input
+     Scanner sc = new Scanner(System.in);   // we use this command to take input from the user.
+     System.out.println("Enter your name:");
+     String Name = sc.nextLine();   // we use sc.nextline while using string
+     System.out.println("Enter your age:");
+     int age = sc.nextInt();   //we use sc.nextInt while using integer.
+     System.out.println("Enter your CGPA:");
+     float percentage = sc.nextFloat();   //we use sc.nextfloat while using float.
+                                 // so there are various things we can add according to the data types we use. //
+
+                                      // USING CONDITIONAL OPERATORS - IF ELSE STATEMENT //
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter your age:");
+     int age = sc.nextInt();   // we use sc.nextInt while using integer
+                                     // we will use some conditional operators eg.{ < , > , <= , >= , == ,!= };
+     if (age >= 20) {
+      System.out.println("WELCOME,YOU ARE ALLOWED FOR VACCINATION:");
+     } else {
+      System.out.println("SORRY,YOU ARE NOT ALLOWED FOR THE VACCINATIONS:");
+
+                                       // USING MAY ELSE IF IN ONE CODE
+      System.out.println("YOU WILL HAVE TO CHECK YOUR TEACHERS COMMENTS BY ENTERING YOUR GRADE:");
+      char Grade = sc.next().charAt(0); // we use sc.next().charAt(0), while using char value
+      if (grade == 'A') {
+       System.out.println("AWESOME.KEEP IT UP");
+      } else if (grade == 'B') {
+       System.out.println("GOOD.BUT NEEDS IMPROVEMENT ");
+      } else if (grade == 'C') {
+       System.out.println("SATISFACTORY.NEEDS HARD-WORK");
+      } else {
+       System.out.println("INVALID INPUT");
+      }
+
+                                            // SWITCH CASE STATEMENT //
+      Scanner SC = new Scanner(System.in);
+      System.out.println("Enter your Grade:");
+      grade = sc.next().charAt(0);
+      switch (grade) {
+       case 'A':
+        System.out.println("OUTSTANDING");
+        break;
+       case 'B':
+        System.out.println("GOOD,BUT NEEDS IMPROVEMENT");
+        break;
+       case 'C':
+        System.out.println("SATISFACTORY");
+        break;
+       default:
+        System.out.println("INVALID");
+
+                                // LOGICAL OPERATORS -  &&(and) , ||(or) , !(not) //
+
+        int age = 52;
+        if (age >= 18 && age <= 52) { //if both the conditions are satisfied then the person can vote
+         System.out.println("you can vote");
+        } else {
+         System.out.println("you cannot vote");
+        }
+
+        int age1 = 1099;
+        if (age > 18 || age <= 52) { //if one of the conditions are satisfied then the person can vote
+         System.out.println("you can vote");
+        } else {
+         System.out.println("you cannot vote");
+        }
+
+        int age2 = 18;
+        if (!(age >= 18)) { // not operator inverses the equation
+         System.out.println("you can vote");
+        } else {
+         System.out.println("you cannot vote");
+        }
+
+                                         //LOOPS (FOR, WHILE  AND DO WHILE)
+        for (int i = 0; i < 10; i++) {
+         System.out.println("charu" + i); //we used  for the loop
+        }
+        int a = 23;
+        while (true) {
+         System.out.println(a); // we used while loop
+         a++;
+         if (a > 100) {
+          if (a == 65) continue; // continue statement will skip tha part where a is 65
+          break;
+
+
+         }
+
+        }
+
+        int a1 = 23;
+        do {
+         System.out.println(a); // we used do while loop
+         a1++;
+        } while (a1 < 100);
+      }
+
+                                                       //ARRAYS //
+                                               //  13 , 29 , 35 , 68 , 95
+
+      int marks[] = new int[5];
+      marks[0] = 13;
+      marks[1] = 29;
+      marks[2] = 35;
+      marks[3] = 68;
+      marks[4] = 95;
+
+      int marks1[] = {13, 29, 35, 68, 95, 29, 68};
+
+      for (int i = 0; i < marks.length; i++) {
+       System.out.println(marks[i]);
+      }
+
+      //MATRIX //
+      int a[][] = new int[2][3];
+      int a1[][] = {
+              {2, 3},
+              {6, 9}
+      };
+      System.out.println(a1[1][0]);
+     }
+    }
+
+*/
+
+ }
+}
